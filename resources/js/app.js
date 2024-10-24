9b3255b7320575060043775094a5527820675f1b
Filaj <107360605+orsoli@users.noreply.github.com>
2024-10-24 13:54:11 +0200

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import "./bootstrap";
+// Import SCSS
+import "~resources/scss/app.scss";
+// Import Bootstrap
+import * as bootstrap from "bootstrap";
+
+import.meta.glob([
+    '../img/**'
+]);
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,4 @@
+{{-- Extends Layouts  --}}
+@extends('layouts.app')
+
+
